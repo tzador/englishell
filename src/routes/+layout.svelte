597c7bc2c8e6668cc690d01ec3b94bf7d85d1481
Tim Zadorozhny <tzador@gmail.com>
2024-11-16 23:17:45 +0100

    -vercel

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,6 @@
   import "papercss/dist/paper.min.css";
   import "../app.css";
   import { dev } from "$app/environment";
-  import { inject } from "@vercel/analytics";
-
-  inject({ mode: dev ? "development" : "production" });
 </script>
 
 <slot />
