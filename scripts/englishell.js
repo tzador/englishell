69e04d31818bac7e47c18f6ddec82ea9a90ef7f4
Tim Zadorozhny <tzador@gmail.com>
2024-11-16 23:11:43 +0100

    auto

--- scripts/englishell.js
+++ scripts/englishell.js
@@ -1,8 +1,7 @@
 #!/usr/bin/env node
 import fetch from "node-fetch";
 
-// const api = "https://englishell.pages.dev/";
-const api = "http://localhost:5173";
+const api = "https://englishell.pages.dev";
 
 const mode = process.argv[2];
 const instructions = process.argv.slice(3).join(" ");
